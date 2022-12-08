3d719dd671f2e585c0e62a5162e1feb860f560d5
Usuario <danimadgom@gmail.com>
2022-12-08 02:46:19 +0100

    V.1

---
+++ app/src/main/java/com/example/tresenraya/Inicio.java
@@ -0,0 +1,40 @@
+package com.example.tresenraya;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Inicio extends AppCompatActivity {
+
+    Button unJugador;
+    Button dosJugadores;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inicio);
+
+        dosJugadores = (Button)findViewById(R.id.dosJugadores);
+
+        dosJugadores.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent dosJugadores = new Intent(Inicio.this, DosJugadores.class);
+                startActivity(dosJugadores);
+            }
+        });
+
+        unJugador = (Button)findViewById(R.id.unJugador);
+
+        unJugador.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent unJugador = new Intent(Inicio.this, UnJugador.class);
+                startActivity(unJugador);
+            }
+        });
+    }
+}
